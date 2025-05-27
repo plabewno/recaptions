8d76c9a5859262c23400e2c1378eef55931be1a7
Plabew <shahriarn@outlook.com>
2025-05-28 01:02:37 +0600

    asfasf

--- src/CaptionedVideo/Page.tsx
+++ src/CaptionedVideo/Page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { AbsoluteFill, interpolate } from "remotion";
+import {
+  AbsoluteFill,
+  interpolate,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
 import { TheBoldFont } from "../load-font";
 import { makeTransform, scale, translateY } from "@remotion/animation-utils";
 import { TikTokPage } from "@remotion/captions";
@@ -14,40 +19,62 @@ const container: React.CSSProperties = {
   height: 150,
 };
 
-const FIXED_FONT_SIZE = 50;
-
 export const Page: React.FC<{
   readonly enterProgress: number;
   readonly page: TikTokPage;
 }> = ({ enterProgress, page }) => {
-  const fontSize = FIXED_FONT_SIZE;
+  const frame = useCurrentFrame();
+  const { fps } = useVideoConfig();
+  const timeInMs = (frame / fps) * 1000;
+
+  const fontSize = 50;
 
   return (
     <AbsoluteFill style={container}>
       <div
         style={{
           fontSize,
-          color: "white",
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-          paddingLeft: "20px",
-          paddingRight: "20px",
-          borderRadius: "10px",
+          color: "black",
+          backgroundColor: "white",
+          paddingLeft: "30px",
+          paddingRight: "30px",
+          borderRadius: "30px",
           transform: makeTransform([
             scale(interpolate(enterProgress, [0, 1], [0.8, 1])),
             translateY(interpolate(enterProgress, [0, 1], [50, 0])),
           ]),
           fontFamily,
           textTransform: "uppercase",
+          display: "inline-block",
+          boxShadow: "8px 8px 10px rgba(0, 0, 0, 0.5)",
         }}
       >
         <span>
           {page.tokens.map((t) => {
+            const startRelativeToSequence = t.fromMs - page.startMs;
+            const revealStartTime = startRelativeToSequence;
+            const revealEndTime = startRelativeToSequence + 200; // Duration for each word to fade in
+
+            const wordOpacityProgress = interpolate(
+              timeInMs,
+              [revealStartTime, revealEndTime],
+              [0, 1],
+              { extrapolateLeft: "clamp", extrapolateRight: "clamp" },
+            );
+            const wordOpacity = interpolate(
+              wordOpacityProgress,
+              [0, 1],
+              [0.5, 1],
+            );
+
             return (
               <span
                 key={t.fromMs}
                 style={{
                   display: "inline",
                   whiteSpace: "pre",
+                  color: "black",
+                  opacity: wordOpacity,
                 }}
               >
                 {t.text}
