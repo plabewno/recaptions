087a285c3f13a2c9b14643e8f2cc4101c266ab77
Plabew <plabew@email.com>
2025-07-07 10:58:34 +0600

    font

--- src/load-font.ts
+++ src/load-font.ts
@@ -15,7 +15,7 @@ export const loadFont = async (): Promise<void> => {
 
   const font = new FontFace(
     CaptionFont,
-    `url('${staticFile("fonts/Rubik-Bold.ttf")}') format('truetype')`,
+    `url('${staticFile("fonts/Rubik-Black.ttf")}') format('truetype')`,
   );
 
   await font.load();
